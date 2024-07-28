f13ea3b94af5823b70ed5a81259ac7e15533c0e7
pederseo <135082830+pederseo@users.noreply.github.com>
2024-07-28 16:05:09 -0400

    Update models.py

--- ejercicio_6/models.py
+++ ejercicio_6/models.py
@@ -1,17 +1,25 @@
+# HERRAMIENTAS
+
 import psycopg2
 import psycopg2.extras
 from config import Conect
 import random
 
-conn = Conect.conexion
+#________________________________________QUERYS______________________________________________________
+
+conn = Conect.conexion # traemos nuestra coneccion
 
 def agregar_entrenadores(nombre, medallas):
+    '''funcion para agregar entrenadores y medallas a la base de datos entrenadores'''
+
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute('INSERT INTO entrenadores (nombre, medallas) VALUES (%s, %s)', (nombre, medallas))
     conn.commit()
     cursor.close()
 
 def mostrar_entrenadores():
+    '''funcion extraer los datos en una lista para llevar al front'''
+
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute("SELECT * FROM entrenadores")
     lista_entrenadores = cursor.fetchall()
@@ -19,12 +27,16 @@ def mostrar_entrenadores():
     return lista_entrenadores
 
 def actualizar_entrenador(id, nombre, medallas):
+    '''funcion actualizar los datos de la tabla entrenadores con datos nuevos'''
+
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute("UPDATE entrenadores SET nombre = %s, medallas = %s WHERE id = %s",(nombre, medallas, id))
     conn.commit()
     cursor.close()
 
 def eliminar_entrenador(id):
+    '''funcion eliminar los datos de la tabla entrenadores'''
+
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute("DELETE FROM entrenadores WHERE id = %s", (id,))
     conn.commit()
@@ -32,6 +44,8 @@ def eliminar_entrenador(id):
 
 #_______________________________________________________________________________________________________________________________
 def mostrar_pokemones():
+    '''funcion extraer los datos de la tabla pokemon para cargar en una lista y mandar al front'''
+
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute("SELECT * FROM pokemones")
     lista_pokemones = cursor.fetchall()
@@ -39,6 +53,8 @@ def mostrar_pokemones():
     return lista_pokemones
 
 def crear_equipos(entrenador, pokemon1, pokemon2):
+    '''funcion cargar datos a la tabla intermedio "equipos" donde relacionamos entrenadores con sus pokemones'''
+
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute('INSERT INTO equipos (id_entrenador, id_pokemon1, id_pokemon2) VALUES (%s, %s, %s)', (entrenador, pokemon1, pokemon2))
     conn.commit()
@@ -46,31 +62,25 @@ def crear_equipos(entrenador, pokemon1, pokemon2):
 
 
 def agregar_resultados(id_entrenador1,id_entrenador2):
-    # Crear un cursor con DictCursor para obtener resultados como diccionarios
-    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    '''funcion agregar los resultados de las batallas a la tabla "batallas" '''
 
-    # Ejecutar las consultas para obtener los nombres de los entrenadores
+
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute(f'SELECT nombre FROM entrenadores WHERE id = {id_entrenador1};')
     entrenador1 = cursor.fetchone()['nombre']
 
     cursor.execute(f'SELECT nombre FROM entrenadores WHERE id = {id_entrenador2};')
     entrenador2 = cursor.fetchone()['nombre']
 
-    # Elegir un ganador aleatoriamente entre los dos entrenadores
     ganador = random.choice([entrenador1, entrenador2])
-
-    # Insertar los resultados en la tabla resultados
     cursor.execute('INSERT INTO resultados (equipo1, equipo2, ganador) VALUES (%s, %s, %s)', (entrenador1, entrenador2, ganador))
-
-    # Confirmar los cambios
     conn.commit()
-
-    # Cerrar el cursor y la conexión
     cursor.close()
-    conn.close()
 
 #__________________________________________________________________________________________________________________________________
 def mostrar_resultados():
+    '''funcion extraer los datos de la tabla batallas para cargar en una lista y mandar al front'''
+    
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cursor.execute("SELECT * FROM resultados")
     lista_resultados = cursor.fetchall()
